b2800509d3da235fce9c07458634baadc49b7d88
Noyal Jacob <johndoe@example.comNoyal>
2021-08-04 22:05:00 -0500

    ruby

--- fundamentals/test.rb
+++ fundamentals/test.rb
@@ -1,1 +1,46 @@
-puts "It's working"
+# puts "It's working"
+
+
+# first_name = "Noyal"
+# last_name = "Jacob"
+
+# puts "First name is %s and last name is %s" % [first_name, last_name]
+
+
+# z = 50
+# puts "Value of z is %d" % z
+
+# number = 15
+# if number % 3 == 0 && number % 5 == 0
+#   puts "FizzBuzz"
+# elsif number % 3 == 0
+#   puts "Fizz"
+# elsif number % 5 == 0
+#   puts "Buzz"
+# end
+# Copy
+
+# if ""
+#     puts "I am positive"
+#   end
+#   if 0
+#     puts "I am positive"
+#   end
+#   if 1
+#     puts "I am positive"
+#   end
+#   Copy
+
+# unless nil
+#     puts "I am negative"
+#   end
+#   unless false
+#     puts "I am negative"
+#   end
+#   Copy
+
+puts "I am positive" if "hello"
+puts "I am positive" if 24
+puts "I am negative" unless nil
+puts "I am negative" unless false
+  Copy
