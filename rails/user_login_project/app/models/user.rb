c9f154641154367dbf04ed278eacaaa1b4e87f33
Noyal Jacob <johndoe@example.comNoyal>
2021-08-06 08:58:25 -0500

    rails assignment

--- rails/user_login_project/app/models/user.rb
+++ rails/user_login_project/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
     EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
-    validates :first_name, :last_name, presence: true, length: { in: 2..20}
+    validates :first_name, :last_name, :email_address, :age, presence: true
+    validates :first_name, :last_name, length: { in: 2..200}
     validates :email_address,  presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+    validates :age, numericality: { only_integer: true, greater_than: 9, less_than: 150 }
 end
