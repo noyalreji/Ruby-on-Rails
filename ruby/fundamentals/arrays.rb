a7750c5cf5330519ebac4915af82340014846dac
Noyal Jacob <johndoe@example.comNoyal>
2021-08-13 10:24:01 -0500

    getting familliar with ruby

---
+++ ruby/fundamentals/arrays.rb
@@ -0,0 +1,24 @@
+a = ["Jacob", "Sara", "Ashley", "Abdu", "Aura", "Brooklyn"]
+b = [2,4,6,8,2,3]
+
+puts a.at(0)
+
+puts a.delete("Sara")
+
+puts a.reverse
+
+puts a.length
+
+puts a.sort
+
+puts b.shuffle
+
+puts b.slice(1..5)
+
+puts a
+
+puts b.join
+
+puts b.insert(1, 3)
+
+puts a.values_at(2..5)
