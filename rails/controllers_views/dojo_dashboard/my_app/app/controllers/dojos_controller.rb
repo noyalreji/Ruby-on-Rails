b4f69f96bd20dc4e926d300a8cf1493eeca07a9e
Noyal Jacob <johndoe@example.comNoyal>
2021-08-17 00:06:23 -0500

    Every routes and buttons working

--- rails/controllers_views/dojo_dashboard/my_app/app/controllers/dojos_controller.rb
+++ rails/controllers_views/dojo_dashboard/my_app/app/controllers/dojos_controller.rb
@@ -4,29 +4,50 @@ class DojosController < ApplicationController
     @number = Dojo.count
   end
 
+  def create
+    @dojo = Dojo.create(dojo_params)
+    if @dojo.valid?
+      redirect_to "/dojos"
+    else
+      flash[:notice] = @dojo.errors.full_messages
+      redirect_to :back
+    end
+  end
+
   def new
-    render "new"
+  end
+
+  def edit
+    @dojo = Dojo.find(params[:id])
   end
 
   def show
     @dojo = Dojo.find(params[:id])
   end
 
-  def edit
-    @dojo = Dojo.find(params[:id])
-    render "edit"
+  # I tried with and without @ symbol
+  def update
+    dojo = Dojo.find(params[:id])
+    dojo.update(dojo_params)
+    dojo.save
+    # MUST HAVE IF STATEMENT or it will throw error if there is no error tp give a full message for
+    puts dojo.errors.full_messages if dojo.errors
+    redirect_to "/dojos"
+    # fail   <<<< Checks params being passed through
   end
 
-  def update
-    @dojo = Dojo.find(params[:id])
-    @dojo.update(dojo_params)
-    if @dojo.valid?
-        redirect_to :root
-    else 
-        flash[:errors] = @dojo.errors.full_messages
-        redirect_to :back 
-    end
+  def destroy
+    dojo = Dojo.find(params[:id])
+    dojo.destroy
+    redirect_to "/dojos"
   end
+  private
+    def dojo_params
+      params.require(:dojo).permit(:branch, :street, :city, :state)
+    end
+
+
+
 end
 
 
